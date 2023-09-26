f8297b2581c6837aa33dc6eda35704613553e2c0
Jordanka Gjeorgieva <jjordanova@hotmail.com>
2023-09-26 13:18:08 +0200

    Added data Json file

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
-// Landing page script
+// Landing page
 
 let dropdwnBtn = document.querySelector("#dropdown_menu_container");
+const dataStored = JSON.parse(localStorage.getItem("artistData"));
+
+async function fetchData() {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+    localStorage.setItem("artistData", JSON.stringify(data));
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+fetchData();
+
 function renderData() {
   let dropDownUl = document.querySelector(".dropdown-menu");
+  console.log(dataStored);
+  dropDownUl.innerHTML = '';
+if(dataStored){
+dataStored.forEach((item) => {
+  let dropdownLi = document.createElement("li");
+  let dropdownItem = document.createElement("a");
+  dropdownItem.classList.add("dropdown-item");
+  dropdownItem.innerText += `${item.name}`;
+  dropdownLi.append(dropdownItem);
 
-  items.forEach((item) => {
-    let dropdownLi = document.createElement("li");
-    let dropdownItem = document.createElement("a");
-    dropdownItem.classList.add("dropdown-item");
-    dropdownItem.innerText += `${item.artist}`;
-    dropdownLi.append(dropdownItem);
+  dropDownUl.append(dropdownLi);
+});
+}else{
+alert("No data found!")
+}
 
-    dropDownUl.append(dropdownLi);
-  });
 }
 dropdwnBtn.addEventListener("click", renderData);
