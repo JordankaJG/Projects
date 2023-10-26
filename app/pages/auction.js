7c39bffa440b12b891432c7f0971d03b60990b3f
Jordanka Gjeorgieva <jjordanova@hotmail.com>
2023-10-26 12:46:17 +0200

    Auction page finished

--- app/pages/auction.js
+++ app/pages/auction.js
@@ -19,9 +19,9 @@ console.log(auctioningItemIndex);
 
 if (
   auctioningItemIndex !== -1 &&
-itemFormLs[auctioningItemIndex].artist === localStorage.getItem("artistName")
+  itemFormLs[auctioningItemIndex].artist === localStorage.getItem("artistName")
 ) {
-  if (!+yourBids[+yourBids.length - 1] && +other[+otherBids.length - 1]) {
+  if (!+yourBids[+yourBids.length - 1] && +otherBids[+otherBids.length - 1]) {
     liveAuctioningItem.innerHTML = `Currently unavailable`;
   } else {
     if (+yourBids[+yourBids.length - 1] > +otherBids[otherBids.length - 1]) {
@@ -49,6 +49,7 @@ function initAuctionPage() {
   auctionInnerElement.classList.remove("d-none");
 
   const activeAuction = items_LS.filter((item) => item.isAuctioning);
+
   if (activeAuction.length === 0) {
     noAuctionElement.classList.remove("d-none");
     auctionInnerElement.classList.add("d-none");
@@ -59,8 +60,8 @@ function initAuctionPage() {
   const input = document.querySelector(".bidAmount");
   const bidBtn = document.querySelector(".bidBtn");
 
-
   const auctioningItemIndex = items_LS.findIndex((item) => item.isAuctioning);
+  console.log(auctioningItemIndex);
 
   if (auctioningItemIndex !== -1) {
     items_LS[auctioningItemIndex].isAuctioning = true;
@@ -74,60 +75,52 @@ function initAuctionPage() {
 
   input.value = auctionStartingPrice.innerText;
 
-
-
-
   if (localStorage.getItem("artistName")) {
     bidBtn.disabled = true;
     bidBtn.innerText = "Disabled for artists";
-bidBtn.style.backgroundColor ="grey" 
+    bidBtn.style.backgroundColor = "grey";
     return;
   } else {
     bidBtn.disabled = false;
     bidBtn.innerText = "Place your bid";
-    bidBtn.style.backgroundColor = "#a16a5e"; 
+    bidBtn.style.backgroundColor = "#a16a5e";
   }
 
-  bidBtn.removeEventListener("click", bid);
-  bidBtn.addEventListener("click", bid);
+  bidBtn.removeEventListener("click", callBidApi);
+  bidBtn.addEventListener("click", callBidApi);
 
-  function bid() {
+  function callBidApi() {
     const bidAmount = input.value;
-    // input.value = +bidAmount + 30;
+    input.value = +bidAmount + 30;
     yourBid.innerHTML += `<li class="bid"><i class="fa-solid fa-money-bill-wave" style="color: #27b42a;"></i> $${+bidAmount}</li>`;
-    otherBid.innerHTML += `<li class="otherBid"> $${
-      +bidAmount + 30
-    } <i class="fa-solid fa-money-bill-wave" style="color: #f05247;"></i></li>`;
+ 
     yourBids.push(+bidAmount);
-    otherBids.push(+bidAmount + 30);
 
-    fetch("https://projects.brainster.tech/bidding/api", {
-      method: "POST",
-      body: JSON.stringify({
-        amount: bidAmount,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
+
+    const formData = new FormData();
+    const bidApi = "https://projects.brainster.tech/bidding/api";
+    formData.append("amount",bidAmount)
+    fetch(bidApi,{
+      method:"POST",
+      body:formData
+    }).then(res=>res.json()).then(data=>{
+     
+      if(data.isBidding){
+        otherBid.innerHTML += `<li class="otherBid">${data.bidAmount}<i class="fa-solid fa-money-bill-wave" style="color: #f05247;"></i></li>`;
+                       otherBids.push(+data.bidAmount);
+      }else{
+
+ biddingsUl.innerHTML = `Bidding is over!!!`;
+      }
     })
-      .then((res) => res.json())
-      .then((data) => {
-        if (!data.isBidding) {
-          countDownEl.innerHTML = `You won the auction!!!`;
-        } else {
-          otherBid.innerHTML += `<li class="otherBid">${data.bidAmount}<li class="bid"><i class="fa-solid fa-user m-1"></i></li>`;
-          otherBids.push(+data.bidAmount);
-          // input.value = +data.bidAmount + 30;
-        }
-      });
-    removeEventListener("click", bid);
+
+    removeEventListener("click", callBidApi);
   }
 }
 
 function startTimer() {
-
-const auctioningItemIndex = itemFormLs.findIndex((item) => item.isAuctioning);
- const timeElement = document.getElementById("time");
+  const auctioningItemIndex = itemFormLs.findIndex((item) => item.isAuctioning);
+  const timeElement = document.getElementById("time");
   let time;
 
   if (+localStorage.getItem("time") > 0) {
@@ -145,7 +138,6 @@ const auctioningItemIndex = itemFormLs.findIndex((item) => item.isAuctioning);
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
 
-   
     timeElement.innerText = `${minutes}:${
       seconds < 10 ? "0" + seconds : seconds
     }`;
@@ -157,7 +149,7 @@ const auctioningItemIndex = itemFormLs.findIndex((item) => item.isAuctioning);
       if (+yourBids[+yourBids.length - 1] > +otherBids[otherBids.length - 1]) {
         items_LS[auctioningItemIndex].priceSold =
           +yourBids[yourBids.length - 1];
-    
+
         timeElement.innerText = `You won the auction! Item sold for $${+yourBids[
           yourBids.length - 1
         ]}`;
@@ -176,4 +168,3 @@ const auctioningItemIndex = itemFormLs.findIndex((item) => item.isAuctioning);
     }
   }, 1000);
 }
-   
